5cdde7557bb2031931d36b1ad7f5a0698e87ee86
harshita.yeole <harshita.yeole@digital.hmrc.gov.uk>
2017-03-11 19:15:31 +0530

    chapter1

--- src/main/scala/chapter2/Problems.scala
+++ src/main/scala/chapter2/Problems.scala
@@ -3,6 +3,47 @@ package chapter2
 /**
   * Created by harshita on 11/3/17.
   */
+
+import scala.annotation.tailrec
+
 class Problems {
+
+  def fib(i: Int): Int = {
+    @tailrec
+    def fibonacci(i: Int, prev: Int, cur: Int): Int = i match {
+      case 0 => prev
+      case 1 => cur
+      case _ => fibonacci(i-1, cur, prev+cur)
+    }
+    fibonacci(i, 0, 1)
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @tailrec
+    def check(n: Int): Boolean = {
+      if(n >= (as.length - 1)) true
+      else if (ordered(as(n), as(n+1))) check(n+1)
+      else false
+    }
+    check(0)
+  }
+
+  //converts a function f of two arguments into a function of one argument that partially applies f
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = {
+    (a: A) => (b: B) => f(a,b)
+  }
+
+  //Implement uncurry, which reverses the transformation of curry.
+  // Note that since => associates to the right, A => (B => C) can be written as A => B => C.
+  //...should convert a function of one argument to a function of two arguments
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = {
+    (a: A, b: B) => f(a)(b)
+  }
+
+  //  Implement the higher-order function that composes two functions.
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C = {
+    a => f(g(a))
+  }
 
 }
--- src/test/scala/chapter2/ProblemsTest.scala
+++ src/test/scala/chapter2/ProblemsTest.scala
@@ -3,6 +3,28 @@ package chapter2
 /**
   * Created by harshita on 11/3/17.
   */
-class ProblemsTest {
+import org.scalatest.{FlatSpec, Matchers}
 
+class ProblemsTest extends FlatSpec with Matchers{
+
+  val fibonacci = new Problems()
+  "Fibonacci" should "return the nth fibonacci number" in{
+    fibonacci.fib(5) should be (5)
+  }
+
+  val isSorted = new Problems()
+  "isSorted" should "return true if array is sorted according to given comparison function" in {
+
+    isSorted.isSorted(Array(1,2,3,4,5), {(a: Int, b: Int) => a < b }) shouldBe true
+    isSorted.isSorted(Array(5,4,3,2), {(a: Int, b: Int) => a < b }) shouldBe false
+    isSorted.isSorted(Array(5,4,3,2), {(a: Int, b: Int) => a > b }) shouldBe true
+  }
+
+  val compose = new Problems()
+  "compose" should "compose two functions" in {
+    val f = (x: Int) => 50 - x
+    val add = (x: Int) => x + 10
+    val sine = compose.compose(add, f)
+    println(sine(30))
+  }
 }
